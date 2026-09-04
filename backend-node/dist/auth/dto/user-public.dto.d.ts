8470310542dbe90814ea5d34f1565221bd656206
NazarenoMoreno1 <nc.moreno@alumno.etec.um.edu.ar>
2026-09-04 11:58:09 -0300

    fix: remover dist y .env de backend-node del control de versiones

--- backend-node/dist/auth/dto/user-public.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../../users/user.entity';
-export declare class UserPublicDto {
-    id: string;
-    email: string;
-    full_name: string | null;
-    created_at: string;
-    updated_at: string;
-    static fromEntity(user: User): UserPublicDto;
-}
--- backend-node/dist/users/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class User {
-    id: string;
-    email: string;
-    passwordHash: string;
-    fullName: string | null;
-    createdAt: Date;
-    updatedAt: Date;
-}
